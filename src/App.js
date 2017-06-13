878bd14c4028631e0bd655ea1b771aa36dfdbd33
vinu123 <wenothe@gmail.com>
2017-06-13 17:54:52 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <center style={{fontSize: '2rem', marginTop: '20px'}}><code>Write your code here</code></center>
+      </div>
+    )
+  }
+}
+
+export default App
