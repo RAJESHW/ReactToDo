ecdb1d9b3ddbed872336fddd074273d0e3e5d509
Rajesh W <rajeshw@xpanxion.co.in>
2017-06-13 21:00:57 +0530

    Partially done add task

--- src/App.js
+++ src/App.js
@@ -2,10 +2,29 @@ import React, { Component } from 'react'
 import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      task : ''
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    this.setState({task : this.refs.addTask.value});
+  }
   render() {
     return (
       <div>
-        <center style={{fontSize: '2rem', marginTop: '20px'}}><code>Write your code here</code></center>
+        <form onSubmit={this.handleSubmit}>
+          <input ref="addTask" type="input" name="addTask" className="addTask"/>
+          <button type="submit" className="btn-addTask">Add Task</button>
+          <div>
+            <ul className="taskList">
+              <li>{this.state.task}</li>
+            </ul>
+          </div>
+        </form>
       </div>
     )
   }
