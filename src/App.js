8482ac7ceea9b6f2cc4efd5df14c794028760bc7
rajeshw <wraejesh@gmail.com>
2017-06-14 00:57:30 +0530

    appended tasks needed more changes in adding task

--- src/App.js
+++ src/App.js
@@ -11,7 +11,13 @@ class App extends Component {
   }
   handleSubmit(e) {
     e.preventDefault();
+    debugger;
     this.setState({task : this.refs.addTask.value});
+    let li = document.createElement('li');
+    li.appendChild(document.createTextNode(this.state.task));
+    const {taskList} = this.refs;
+    taskList.appendChild(li);
+    this.refs.addTask.value = '';
   }
   render() {
     return (
@@ -20,8 +26,8 @@ class App extends Component {
           <input ref="addTask" type="input" name="addTask" className="addTask"/>
           <button type="submit" className="btn-addTask">Add Task</button>
           <div>
-            <ul className="taskList">
-              <li>{this.state.task}</li>
+            <ul ref="taskList">
+              {/*<List tasks={this.state.task}/>*/}
             </ul>
           </div>
         </form>
@@ -29,5 +35,12 @@ class App extends Component {
     )
   }
 }
+
+class List extends Component {
+  render() {
+    return <li> {this.props.tasks} </li>
+  }
+}
+
 
 export default App
